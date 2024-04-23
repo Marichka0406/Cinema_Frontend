253c1afebb9587f09f2b19fe12bdb148247a2e93
Анна-Марія <anna-mariia.kurhanevych.pz.2021@lpnu.ua>
2024-04-23 19:36:32 +0300

    Added styles for login form

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,101 @@
-import { styles } from "./LoginPage.styles" 
+import React, { useState } from "react";
+import {
+  Box,
+  TextField,
+  Button,
+  Grid,
+  Paper,
+  Typography,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { styles } from "./LoginPage.styles";
 
 const LoginPage = ({ onLogin }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleLogin = () => {
+    // Викликається, коли користувач натискає кнопку "Увійти"
+    // Тут можна виконати логіку перевірки інформації для входу і передати результат на зовнішню функцію onLogin
+    onLogin(true);
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
-    <>Login Page</>
+    <Box sx={styles.formWrapper}>
+      <Grid container justifyContent="center">
+        <Grid item xs={12} md={6}>
+          <Paper elevation={3} sx={styles.formContainer}>
+            <Typography variant="h5" sx={styles.title}>
+              Login
+            </Typography>
+            <FormControl fullWidth sx={styles.input} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-email">
+                Email
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-email"
+                type="email"
+                label="Email*"
+                onChange={handleChangeEmail}
+                
+              />
+            </FormControl>
+            <FormControl fullWidth sx={styles.input} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+                onChange={handleChangePassword}
+              />
+            </FormControl>
+            <Button
+              variant="contained"
+              onClick={handleLogin}
+              sx={styles.button}
+            >
+              Login
+            </Button>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
--- src/pages/LoginPage/LoginPage.styles.js
+++ src/pages/LoginPage/LoginPage.styles.js
@@ -1,3 +1,79 @@
 export const styles = {
-    
+  formWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    background: 'linear-gradient(to right, #B71C1C, #880E4F, #673AB7)',
+  },
+  formContainer: {
+    p: '20px',
+    maxWidth: '400px',
+    m: 'auto',
+    backgroundColor: '#E3C6E7',
+    boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.7)',
+    borderRadius: '14px',
+  },
+  title: {
+    textAlign: 'center',
+    mb: '30px',
+    color: '#4e342e',
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+    fontSize: '40px',
+    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.1)',
+    background: 'linear-gradient(to right, #B71C1C, #880E4F, #673AB7)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+  },
+  input: {
+    width:'100%',
+    backgroundColor: '#EDF5FF',
+    mb: '35px',
+    '& label': {
+      color: '#B71C1C',
+      fontSize: '17px',
+      fontWeight: '600',
+    },
+    '& label.Mui-focused': {
+      color: '#673AB7',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#B71C1C',
+        borderWidth: '3px',
+        borderStyle: 'solid',
+      },
+      '&:hover fieldset': {
+        borderColor: '#B71C1C',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#673AB7',
+      },
+      '& .MuiInputBase-input': {
+        fontFamily: 'Montserrat',
+        fontSize: '16px',
+        fontWeight: '500',
+      },
+      '& .MuiIconButton-root': {
+        color: '#B71C1C', 
+      },
+      '&.Mui-focused .MuiIconButton-root': {
+        color: '#673AB7', 
+      },     
+    },
+  },
+  button: {
+    background: 'linear-gradient(to right, #B71C1C, #880E4F, #673AB7)',
+    color: '#fff',
+    width: '100%',
+    '&:hover': {
+      background: 'linear-gradient(to right, #77052F , #880E4F, #0A0A53)',
+    },
+    borderRadius: '20px',
+    fontFamily: 'Montserrat',
+    fontWeight: '500',
+    fontSize: '18px',
+  },
 };
